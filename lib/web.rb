1dbf9fb767d1a8919e50f56185b63daade5cb199
Jakub Jirutka <jakub@jirutka.cz>
2014-03-21 23:48:45 +0100

    Add resource for redirect to search.maven.org

--- lib/web.rb
+++ lib/web.rb
@@ -3,6 +3,7 @@ require_relative 'sonatype'
 require_relative 'shields'
 
 DEFAULT_SUBJECT = 'maven central'
+MAVEN_SEARCH_URI = 'http://search.maven.org'
 PROJECT_SITE = 'https://github.com/jirutka/maven-badges'
 
 configure :production do
@@ -15,6 +16,7 @@ get '/' do
   "Nothing is here, see #{PROJECT_SITE}."
 end
 
+# Returns badge image
 get '/maven-central/:group/:artifact/badge.?:format?' do |group, artifact, format|
   halt 415 unless ['svg', 'png'].include? format
 
@@ -30,6 +32,16 @@ get '/maven-central/:group/:artifact/badge.?:format?' do |group, artifact, forma
   end
 
   Shields.badge_image subject, version, color, format
+end
+
+# Redirects to artifact's page on maven.org
+get '/maven-central/:group/:artifact/?' do |group, artifact|
+  begin
+    version = Sonatype.last_artifact_version(group, artifact)
+    redirect to "#{MAVEN_SEARCH_URI}/#artifactdetails|#{group}|#{artifact}|#{version}|"
+  rescue NotFoundError
+    redirect to "#{MAVEN_SEARCH_URI}/#search|ga|1|g:\"#{group}\" AND a:\"#{artifact}\""
+  end
 end
 
 error do
